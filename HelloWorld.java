1c6cd56e68ec210c83c5958c1b1635c4fe1c047e
ZhNi5 <zhiznnika5>
2023-10-10 20:50:51 +0300

    refactor message System.out.

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello world from Veronika!");
+
+        System.out.println("Hello world from Veronika 777!");
     }
 }
